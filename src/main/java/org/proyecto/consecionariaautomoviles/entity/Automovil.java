fd5a0a1d8b423d7d7dcf72b0296db90c8047ca90
Renzo Guerra <renzoguerralatt@gmail.com>
2026-03-30 16:40:23 -0300

    ADD: Automovil entity

---
+++ src/main/java/org/proyecto/consecionariaautomoviles/entity/Automovil.java
@@ -0,0 +1,34 @@
+package org.proyecto.consecionariaautomoviles.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Automovil {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String modelo;
+    @NotBlank
+    private String marca;
+    @NotBlank
+    private String motor;
+    @NotBlank
+    private String color;
+    @NotBlank
+    @Length(min = 6, max = 7)
+    @Pattern(regexp = "^([a-zA-Z]{3}\\d{3}|[a-zA-Z]{2}\\d{3}[a-zA-Z]{2})$", message = "Por favor ingrese una patente válida")
+    @Column(unique = true)
+    // Acepta el formato de patentes viejas como modernas
+    private String patente;
+
+}
